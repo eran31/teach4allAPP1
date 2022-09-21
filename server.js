e0c9e942db6fe7776b6dc13a0f9df353109a7f1a
chicool <eran@apptodate.co.il>
2022-09-21 10:22:15 +0300

    update server.js

--- server.js
+++ server.js
@@ -58,7 +58,13 @@ error => {
   }
   */
 
-app.use('/', express.static('client/build'));
+//app.use('/', express.static('client/build'));
+app.use(express.static(path.join(__dirname,"/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/client/build/index.html")
+  );
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
